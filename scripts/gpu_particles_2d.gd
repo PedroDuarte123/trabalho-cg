c4d0a74d993817503982a22cfbb18588f14371dc
gabriel-andrade-alves <gabriel.dandrade.alves@gmail.com>
2026-06-05 13:01:58 -0300

    fix: particulas-folhas

--- scripts/gpu_particles_2d.gd
+++ scripts/gpu_particles_2d.gd
@@ -2,5 +2,7 @@ extends GPUParticles2D
 
 @onready var camera = get_viewport().get_camera_2d()
 
-func _process(delta):
-	global_position = camera.global_position
+func _process(_delta: float) -> void:
+	var cam := get_viewport().get_camera_2d()
+	if is_instance_valid(cam):
+		global_position = cam.global_position
